e08bbf128d5619a4b53c5c1b08fa64b502d5fe7e
Potato987. <z.p.a.toto@gmail.com>
2025-07-20 14:26:14 +1000

    Create ui.py

---
+++ ui.py
@@ -0,0 +1,13 @@
+class UI:
+    def draw_bounding_boxes(self, qr_tuple) -> None:
+        '''
+        draws a bounding box overlay on screen for detected qr codes
+        '''
+        pass
+    
+    def display_info(self, qr_tuple) -> None:
+        '''
+        displays decoded info near the bounding boxes for each qr code
+        '''
+        pass
+
